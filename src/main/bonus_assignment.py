465dfa8a790537983122d73421ce11aab5e390ca
Daina Diedrick <122504389+diamond5702@users.noreply.github.com>
2023-04-19 18:52:25 -0400

    Update bonus_assignment.py

--- src/main/bonus_assignment.py
+++ src/main/bonus_assignment.py
@@ -2,3 +2,9 @@ import numpy as np
 
 # Question 1 - The number of iterations it takes gauss-seidel to converge:
 
+
+tol= 1e-6
+A = np.array([[3, 1, 3],
+              [1, 4, 1],
+              [2, 3, 7]])
+b = np.array([1, 3, 0])
